fcf0d4ac5630ce02d6b53ccca0bd4e04270e7602
Jip van de Vijfeijke <jip.vandevijfeijke@altran.com>
2020-09-24 14:37:24 +0200

    #11 Added some comment about Tycho and Java relation
    	And suppress Maven download cli output

--- acidcli/mde-build.sh
+++ acidcli/mde-build.sh
@@ -8,4 +8,8 @@ ACIDCLI_MAVEN_POM="${SCRIPT_DIR}/../pom.xml"
 ACIDCLI_MAVEN_SETTINGS="${SCRIPT_DIR}/../settings.xml"
 
 # Start the build.
-mvn package -s ${ACIDCLI_MAVEN_SETTINGS} -f ${ACIDCLI_MAVEN_POM} -DskipTests=true -e -U -B
+# -e Produce execution error messages
+# -U Forces a check for missing releases and updated snapshots on remote repositories
+# -B Run in non-interactive (batch) mode (disables output color; supresses download progress)
+# -Dorg.slf4j.simpleLogger.log.org.apache.maven.cli.transfer=warn Supresses download INFO messages
+mvn package -s ${ACIDCLI_MAVEN_SETTINGS} -f ${ACIDCLI_MAVEN_POM} -DskipTests=true -e -U -B -Dorg.slf4j.simpleLogger.log.org.apache.maven.cli.transfer=warn
