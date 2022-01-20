29e492706a673cb61540490316437c6683ffd85e
yblankenstein <yuri.blankenstein@altran.com>
2022-01-20 10:19:42 +0100

    #14 A bit more verbose Periksa example

--- plugins/com.altran.ec.mde.skeleton.espilce.periksa/src/com/altran/ec/mde/skeleton/espilce/periksa/StatemachineValidations.java
+++ plugins/com.altran.ec.mde.skeleton.espilce.periksa/src/com/altran/ec/mde/skeleton/espilce/periksa/StatemachineValidations.java
@@ -1,12 +1,12 @@
 package com.altran.ec.mde.skeleton.espilce.periksa;
 
+import org.eclipse.xtext.example.fowlerdsl.statemachine.State;
 import org.eclipse.xtext.example.fowlerdsl.statemachine.Statemachine;
 import org.eclipse.xtext.example.fowlerdsl.statemachine.StatemachinePackage;
 import org.espilce.periksa.validation.Check;
 import org.espilce.periksa.validation.CheckContext;
 import org.espilce.periksa.validation.ValidationLibrary;
 
-// tag::doc-espilce-periksa[]
 /**
  * This class shows an example of how easy it is to add model validations, using
  * Espilce Periksa. This class is registered as a validator for the
@@ -14,6 +14,7 @@ import org.espilce.periksa.validation.ValidationLibrary;
  * <tt>pom.xml</tt> file in the base directory of this plugin. For more
  * information, please read the Espilce Periksa programmer guide.
  */
+//tag::doc-espilce-periksa[]
 public class StatemachineValidations {
 	/**
 	 * The {@link Check @Check} annotation registers this method to validate all
@@ -26,10 +27,21 @@ public class StatemachineValidations {
 	@Check // <1>
 	public static void checkDuplicates(Statemachine statemachine, CheckContext context) {
 		ValidationLibrary.checkDuplicateValue(statemachine.getCommands(), StatemachinePackage.Literals.COMMAND__NAME, context); // <2>
-		ValidationLibrary.checkDuplicateValue(statemachine.getCommands(), StatemachinePackage.Literals.COMMAND__CODE, context);
+		// See java source for more validations...
+// end::doc-espilce-periksa[]
+		// These additional validations are not part of the documentation
 		ValidationLibrary.checkDuplicateValue(statemachine.getEvents(), StatemachinePackage.Literals.EVENT__NAME, context);
-		ValidationLibrary.checkDuplicateValue(statemachine.getEvents(), StatemachinePackage.Literals.EVENT__CODE, context);
 		ValidationLibrary.checkDuplicateValue(statemachine.getStates(), StatemachinePackage.Literals.STATE__NAME, context);
+		ValidationLibrary.checkDuplicateValue(statemachine.getCommands(), StatemachinePackage.Literals.COMMAND__CODE, context);
+		ValidationLibrary.checkDuplicateValue(statemachine.getEvents(), StatemachinePackage.Literals.EVENT__CODE, context);
+// tag::doc-espilce-periksa[]
+	}
+
+	@Check
+	public static void checkNameStartsWithCapital(State state, CheckContext context) {
+		if (!state.getName().isEmpty() && state.getName().charAt(0) != Character.toUpperCase(state.getName().charAt(0))) {
+			context.getReport().info("Name should start with upper case", StatemachinePackage.Literals.STATE__NAME); // <3>
+		}
 	}
 }
 // end::doc-espilce-periksa[]
